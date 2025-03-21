1f5137a91ec3b8b54d6d248d97cc37a4bffeefc9
estebanbecerra <92780395+estebanbecerra@users.noreply.github.com>
2025-03-21 10:38:53 +0100

    Add files via upload

---
+++ sh/train/squeezenet_gpu_entrenamiento.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p cola02
+#SBATCH --gres=gpu:1 #Si se usase la cola02 (con GPU)
+#SBATCH -c 1
+#SBATCH --mem=4G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=01:00:00
+#SBATCH --job-name=squeezenet_gpu # Nombre del job
+#SBATCH --output=/home/estebanbecerraf/outputs/train/squeezenet_gpu_%j.log # Archivo de salida
+
+# Ejecutar el código Python dentro del contenedor Apptainer
+apptainer exec --writable-tmpfs --nv /software/singularity/Informatica/mia-idl-apptainer/mia_idl_2.1.sif \
+    accelerate launch --config_file /home/estebanbecerraf/config/config_gpubase.yaml \
+    /home/estebanbecerraf/scripts/train/entrenamiento_squeezenet_gpu.py
