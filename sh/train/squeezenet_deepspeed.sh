8ae571cc384ea2b8198d890c13e34cc73f70043a
estebanbecerra <92780395+estebanbecerra@users.noreply.github.com>
2025-03-30 18:19:31 +0200

    Update squeezenet_deepspeed.sh

--- sh/train/squeezenet_deepspeed.sh
+++ sh/train/squeezenet_deepspeed.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH -p cola02
-#SBATCH --gres=gpu:1 #Si se usase la cola02 (con GPU)
+#SBATCH --gres=gpu:1
 #SBATCH -c 1
 #SBATCH --mem=4G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
-#SBATCH --job-name=squeezenet_deepspeed # Nombre del job
-#SBATCH --output=../../outputs/train/squeezenet_gpu_%j.log # Archivo de salida
+#SBATCH --job-name=squeezenet_gpu_ds
+#SBATCH --output=../../outputs/train/squeezenet_deepspeed_%j.log
 
-# Ejecutar el código Python dentro del contenedor Apptainer
 apptainer exec --writable-tmpfs --nv /software/singularity/Informatica/mia-idl-apptainer/mia_idl_2.2.sif \
-    accelerate launch --config_file ../../config/config_gpubase.yaml \
-    /home/estebanbecerraf/scripts/train/entrenamiento_squeezenet_gpu.py
+    deepspeed ../../scripts/train/train_squeezenet_deepspeed.py \
+    --deepspeed --deepspeed_config ../../config/deepspeed_config.json
